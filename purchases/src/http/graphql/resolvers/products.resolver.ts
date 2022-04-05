7d81f617c2a4ead1864945de9d5da57720b3005e
Marcos Kloss <marcos.kloss@develcode.com.br>
2022-04-05 11:28:06 -0300

    create and list products

---
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -0,0 +1,23 @@
+import { UseGuards } from '@nestjs/common';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+
+import { ProductsService } from 'src/services/products.service';
+import { CreateProductInput } from '../inputs/create-product-input';
+import { Product } from '../models/Product';
+
+@Resolver(() => Product)
+export class ProductsResolver {
+  constructor(private productsService: ProductsService) {}
+
+  @Query(() => [Product])
+  async products() {
+    return this.productsService.listAllProducts();
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @Mutation(() => Product)
+  async createProduct(@Args('data') data: CreateProductInput) {
+    return this.productsService.createProduct(data);
+  }
+}
--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -5,7 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 import path from 'node:path';
 
 import { DatabaseModule } from '../database/database.module';
-import { TestResolver } from './test.resolver';
+import { ProductsResolver } from './graphql/resolvers/products.resolver';
+import { ProductsService } from 'src/services/products.service';
 
 @Module({
   imports: [
@@ -16,6 +17,6 @@ import { TestResolver } from './test.resolver';
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
-  providers: [TestResolver],
+  providers: [ProductsResolver, ProductsService],
 })
 export class HttpModule {}
