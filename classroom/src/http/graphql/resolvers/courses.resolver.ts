b54dad6233b0a94bcc6968a8f13f5370ad096493
Marcos Kloss <marcos.kloss@develcode.com.br>
2022-04-05 17:02:27 -0300

    list courses

--- classroom/src/http/graphql/resolvers/courses.resolver.ts
+++ classroom/src/http/graphql/resolvers/courses.resolver.ts
@@ -1,5 +1,16 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Query, Resolver } from '@nestjs/graphql';
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { CoursesService } from 'src/services/courses.service';
 import { Course } from '../models/Course';
 
 @Resolver(() => Course)
-export class CoursesResolver {}
+export class CoursesResolver {
+  constructor(private coursesService: CoursesService) {}
+
+  @Query(() => [Course])
+  @UseGuards(AuthorizationGuard)
+  async courses() {
+    return this.coursesService.listAllCourses();
+  }
+}
