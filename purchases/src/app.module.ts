75bd57e334b8a9003ccd7fcaa894ce473999be75
Marcos Kloss <marcos.kloss@develcode.com.br>
2022-04-05 09:51:34 -0300

    configure auth middleware and graphQL

--- purchases/src/app.module.ts
+++ purchases/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { PrismaService } from './database/prisma/prisma.service';
 import { HttpModule } from './http/http.module';
 
 @Module({
   imports: [DatabaseModule, HttpModule],
-  controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
